4b769bba7ccf954132ff85ae59f5c07ae415d13d
Олександр Канюка <137859612+crusaider423@users.noreply.github.com>
2024-03-25 16:06:34 +0200

    update

---
+++ decorator/decorator.js
@@ -0,0 +1,10 @@
+export const decorator = (controller) => {
+  const func = async (req, res, next) => {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+  return func;
+};
